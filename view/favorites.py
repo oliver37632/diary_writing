d335ecf1c3fc357a9e6adf7c298da7a5570be60a
김승진 <klli1naver@gmail.com>
2021-11-11 17:22:56 +0900

    Create view favorites

---
+++ view/favorites.py
@@ -0,0 +1,13 @@
+from flask_restful import Resource
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from contorller.favorites import favorites
+
+
+class Favorites(Resource):
+    @jwt_required()
+    def post(self, id):
+        token = get_jwt_identity()
+        return favorites(
+            id=id,
+            token=token
+        )
