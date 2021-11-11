14a9382464982bceca59c998124802738eea9f89
김승진 <klli1naver@gmail.com>
2021-11-11 17:22:38 +0900

    Create view auth

---
+++ view/auth.py
@@ -0,0 +1,28 @@
+from flask import request
+from flask_restful import Resource
+from contorller.auth import sigup, login
+
+
+class SigUp(Resource):
+    def post(self):
+        nick = request.json['nick']
+        name = request.json['name']
+        password = request.json['password']
+
+        return sigup(
+                nick=nick,
+                name=name,
+                password=password
+                     )
+
+
+
+class Login(Resource):
+    def post(self):
+        nick = request.json['nick']
+        password = request.json['password']
+
+        return login(
+            nick=nick,
+            password=password
+        )
