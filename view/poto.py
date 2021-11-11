720946b799b6c16aa9ceb76c80c8eb4056127926
김승진 <klli1naver@gmail.com>
2021-11-11 17:22:18 +0900

    Create view poto

---
+++ view/poto.py
@@ -0,0 +1,26 @@
+from flask_restful import Resource
+from flask import request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from contorller.poto import upload, download
+
+
+class Upload(Resource):
+    @jwt_required()
+    def post(self):
+        name = request.args.get("name")
+        inherence = request.args.get("inherence")
+
+        return upload(
+            name=name,
+            inherence=inherence
+        )
+
+
+class Download(Resource):
+    @jwt_required()
+    def get(self):
+        inherence = request.args.get("inherence")
+
+        return download(
+            inherence=inherence
+        )
