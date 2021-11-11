0962eac82c45e94c1e19b1efc2fa2ac3a1209c43
김승진 <klli1naver@gmail.com>
2021-11-11 17:24:46 +0900

    Create contorller auth

---
+++ contorller/auth.py
@@ -0,0 +1,51 @@
+from model import session_scope
+from flask import abort
+from model.user import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import timedelta
+from flask_jwt_extended import create_access_token, create_refresh_token
+
+
+def sigup(nick, name, password):
+    with session_scope() as session:
+
+        new_sigup = User(
+            nick=nick,
+            name=name,
+            password=generate_password_hash(password)
+        )
+        session.add(new_sigup)
+        session.commit()
+
+        return {
+                   "message": "success"
+               }, 201
+
+
+
+def login(nick, password):
+    with session_scope() as session:
+        user = session.query(User).filter(User.nick == nick)
+
+        if not user.scalar():
+            abort(409, 'user id code does not match')
+
+        user = user.first()
+        check_user_pw = check_password_hash(user.password, password)
+
+        if not check_user_pw:
+            abort(409, 'user password code does not match')
+
+        access_expires_delta = timedelta(minutes=60)
+        refresh_expires_delta = timedelta(weeks=1)
+
+        access_token = create_access_token(expires_delta=access_expires_delta,
+                                           identity=nick
+                                           )
+        refresh_token = create_refresh_token(expires_delta=refresh_expires_delta,
+                                             identity=nick
+                                             )
+        return {
+                   'access_token': access_token,
+                   'refresh_token': refresh_token
+               }, 201
