6555bb0a844e603fed92321692f8f118395b100f
김승진 <klli1naver@gmail.com>
2021-12-09 15:39:00 +0900

    fix favorites

--- server/contorller/favorites.py
+++ server/contorller/favorites.py
@@ -8,7 +8,7 @@ def favorites(id, token):
     with session_scope() as session:
         post = session.query(Post).filter(Post.user_nick == token).first()
         if post:
-            post = session.query(Post).filter(Post.id_pk == id).first()
+            post = session.query(Post).filter(Post.inherence == id).first()
             if post:
                 if post.Favorites == 1:
                     post.Favorites = 0
@@ -59,7 +59,7 @@ def delete_favorites(id ,token):
                        "message": "user id not match"
                    }, 400
 
-        post = session.query(Post).filter(Post.id_pk == id).first()
+        post = session.query(Post).filter(Post.inherence == id).first()
 
         if not post:
             return {
